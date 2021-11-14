b184655e1c993b8214fb7118f1b35d89157567ea
CassianoJunior <cassianojuniorww@gmail.com>
2021-11-14 13:02:52 -0300

    Modificando footer para adicionar o link para minha pagina do github

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,14 @@
-import { Text, Flex } from '@chakra-ui/react';
+import { Text, Flex, Link } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const Footer = () => (
   <Flex aling="center" justify="center" mt={4}>
-    <Text as="p">Made with love &#128156; by Cassiano Junior</Text>
+    <Text as="p">
+      Made with love &#128156; by{' '}
+      <Link href="https://github.com/CassianoJunior" isExternal>
+        Cassiano Junior <ExternalLinkIcon mx={1} />
+      </Link>
+    </Text>
   </Flex>
 );
 
